193bddccbd3f156dc3e6059ac922214d2f9d1fb4
Richard Mathot <richard.mathot@gmail.com>
2015-09-12 15:45:09 +0200

    First (and probably last) commit

---
+++ README.md
@@ -0,0 +1,8 @@
+A Photo Renamer for after the holidays ;-)
+==========================================
+
+I was fed up with these cameras that are not able to name picture files in a clean way. This script roughly renames `.jpg` files after the date and hour they were shooted. 
+
+DISCLAIMER: This script was written in less than one hour... If I were you, I wouldn't use it. 
+
+LICENSE: This code is placed under WTFPLv2 (See the `LICENSE.txt` file.); I don't care if you copy-paste it!
---
+++ photo_renamer.py
@@ -0,0 +1,33 @@
+#!/bin/env python2
+# -*- coding: utf-8 -*-
+
+import datetime
+import os
+import PIL.Image
+import PIL.ExifTags
+import sys
+
+
+def main(args):
+    folderpath = os.path.abspath(args[0])
+    print("Working with directory: %s" % folderpath)
+
+    pictures = [f for f in os.listdir(folderpath) if f.endswith('.JPG')]
+    pictures.sort()
+
+    for pic in pictures:
+        img = PIL.Image.open(os.path.join(folderpath, pic))
+        raw_time = img._getexif()[306]
+        img.close()
+        
+        converted_time = datetime.datetime.strptime(raw_time, '%Y:%m:%d %H:%M:%S')
+        newname = str(converted_time) + '.jpg'
+
+        os.rename(os.path.join(folderpath, pic), os.path.join(folderpath, newname))
+
+        print('%s -> %s' % (pic, newname))
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    main(args)
