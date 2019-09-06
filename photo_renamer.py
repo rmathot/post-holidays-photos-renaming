e4dd4706414247c36666686444c1aafd0c063226
Richard Mathot <richard.mathot@gmail.com>
2019-09-06 16:33:19 +0200

    v2

--- photo_renamer.py
+++ photo_renamer.py
@@ -1,4 +1,4 @@
-#!/bin/env python2
+#!/bin/env python3
 # -*- coding: utf-8 -*-
 
 import datetime
@@ -11,6 +11,12 @@ import sys
 def main(args):
     folderpath = os.path.abspath(args[0])
     print("Working with directory: %s" % folderpath)
+    try:
+        timezone_offset = int(args[1])
+    except IndexError:
+        timezone_offset = 0
+    except ValueError:
+        timezone_offset = 0
 
     pictures = [f for f in os.listdir(folderpath) if f.endswith('.JPG')]
     pictures.sort()
@@ -19,8 +25,11 @@ def main(args):
         img = PIL.Image.open(os.path.join(folderpath, pic))
         raw_time = img._getexif()[306]
         img.close()
-        
+
         converted_time = datetime.datetime.strptime(raw_time, '%Y:%m:%d %H:%M:%S')
+        if timezone_offset:
+            print("Offsetting original time by %d hours" % timezone_offset)
+            converted_time = converted_time + datetime.timedelta(hours=timezone_offset)
         newname = str(converted_time).replace(':', '.') + '.jpg'
 
         os.rename(os.path.join(folderpath, pic), os.path.join(folderpath, newname))
